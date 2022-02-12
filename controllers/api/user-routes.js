e32f21b7e011dbe98dc10ff988225e0f3f938215
KJ Jackson <kjjackson619@yahoo.com>
2022-02-12 12:14:33 -0500

    authentication

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,72 +60,70 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address!' });
-            return;
-        }
-        //res.json({ user: dbUserData });
-
-        const validPassword = dbUserData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!' });
-            return;
-        }
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-});
-
-router.put('/:id', (req, res) => {
-    User.update(req.body, {
-        individualHooks: true,
-        where: {
-            id: req.params.id
-        }
-    })
         .then(dbUserData => {
-            if (!dbUserData[0]) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
 
-router.delete('/:id', (req, res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(400).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData)
+                res.json(dbUserData);
+            });
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
 
-module.exports = router;
+    router.post('/login', (req, res) => {
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+
+            });
+        });
+
+
+        router.put('/:id', (req, res) => {
+            User.update(req.body, {
+                individualHooks: true,
+                where: {
+                    id: req.params.id
+                }
+            })
+                .then(dbUserData => {
+                    if (!dbUserData[0]) {
+                        res.status(404).json({ message: 'No user found with this id' });
+                        return;
+                    }
+                    res.json(dbUserData);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        });
+
+        router.delete('/:id', (req, res) => {
+            User.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                        res.status(400).json({ message: 'No user found with this id' });
+                        return;
+                    }
+                    res.json(dbUserData)
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        });
+
+        module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
